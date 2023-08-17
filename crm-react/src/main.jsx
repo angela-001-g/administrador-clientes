57882ac1f7e093430db019e00162d7e605934714
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-17 17:00:47 -0500

    usando el router y definiendo primeras rutas

--- crm-react/src/main.jsx
+++ crm-react/src/main.jsx
@@ -1,10 +1,19 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    { 
+        path: '/', 
+        element: <h1>Inicio</h1>
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+<RouterProvider 
+    router={router}
+/>    
 
 )
