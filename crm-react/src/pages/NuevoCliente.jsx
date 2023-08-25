0fe53dd7ad9b1121ea89cdf0c6254950ac50c59c
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-24 20:56:33 -0500

    añadir validación

--- crm-react/src/pages/NuevoCliente.jsx
+++ crm-react/src/pages/NuevoCliente.jsx
@@ -1,20 +1,32 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useNavigate, Form } from "react-router-dom"
+import { useNavigate, Form, useActionData } from "react-router-dom"
 import Formulario from "../components/Formulario";
+import Error from "../components/Error";
 
 export async function action({request}) {
   const formData = await request.formData()
 
   const datos = Object.fromEntries(formData)
-  console.log(datos)
+//  Validación 
+const errores = [];
+if(Object.values(datos).includes('')){
+  errores.push('Todos los campos son obligatorios')
+}
+
+// Retornar datos si hay errores 
+if(Object.keys(errores).length){
+  console.log('Si hay errores')
+}
+
   return ''
 }
 
 function NuevoCliente() {
 
+  const errores = useActionData();
   const navigate = useNavigate();
 
-
+  console.log(errores)
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo cliente</h1>
@@ -30,6 +42,9 @@ function NuevoCliente() {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+
+            {errores?.length && errores.map((error, i) => <Error key={i}>{error}<Error/> )}
+
             <Form
               method="post"
             >
