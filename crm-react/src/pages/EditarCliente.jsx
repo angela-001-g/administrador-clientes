9f90f4ef8bdf793c1bf7c66e235e9dad17a37c7a
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-29 21:26:43 -0500

    accion para eliminar clientes

--- crm-react/src/pages/EditarCliente.jsx
+++ crm-react/src/pages/EditarCliente.jsx
@@ -1,8 +1,12 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-control-regex */
 /* eslint-disable react-refresh/only-export-components */
 
-import { Form, useNavigate, useLoaderData } from "react-router-dom"
-import { obtenerCliente } from "../data/clientes"
+import { Form, useNavigate, useLoaderData, redirect, useActionData } from "react-router-dom"
+import { obtenerCliente, actualizarCliente } from "../data/clientes"
 import Formulario from "../components/Formulario"
+import Error from "../components/Error"
+
 
 export async function loader({params}){
   const cliente = await obtenerCliente(params.clienteId)
@@ -16,10 +20,41 @@ export async function loader({params}){
   return cliente
 }
 
+export async function action({request, params}){
+  const formData = await request.formData()
+
+  const datos = Object.fromEntries(formData)
+
+  const email = formData.get('email')
+
+
+//  Validación 
+const errores = [];
+if(Object.values(datos).includes('')){
+  errores.push('Todos los campos son obligatorios')
+}
+
+let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+  if(!regex.test(email)){
+    errores.push('Email no válido')
+  }
+  // Retornar datos si hay errores 
+  if(Object.keys(errores).length){
+    return errores
+  }
+
+  // Actualizar el cliente
+  await actualizarCliente(params.clienteId, datos)
+
+  return redirect('/')
+}
+
 function EditarClientes() {
   const navigate = useNavigate();
   const cliente = useLoaderData(); 
-  console.log(cliente)
+  const errores = useActionData()
+ 
 
   return (
       <>
@@ -37,7 +72,7 @@ function EditarClientes() {
 
             <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
 
-                  {/* {errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error> )} */}
+                  {errores?.length && errores.map( (error, i) => <Error key={i}>{error}</Error> )}
 
                   <Form
                     method="post"
@@ -49,7 +84,7 @@ function EditarClientes() {
                     <input 
                       type="submit" 
                       className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
-                      value="Registrar Cliente"
+                      value="Guardar Cambios"
                     />
                   </Form>
             </div>
