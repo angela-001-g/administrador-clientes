e19c4955969b55578a18396d5b7b6c656e7f8f4d
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-29 21:52:38 -0500

    eliminando clientes

--- crm-react/src/components/Cliente.jsx
+++ crm-react/src/components/Cliente.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
 
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Form, redirect } from "react-router-dom"
+import { eliminarCliente } from "../data/clientes"
 
+export async function action({params}){
+    await eliminarCliente(params.clienteId)
+    return redirect('/')
+}
 
 function Cliente({cliente}) {
   const navigate = useNavigate()
@@ -31,10 +37,22 @@ function Cliente({cliente}) {
                 className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
                 onClick={() => navigate(`/clientes/${id}/editar`)}
             >Editar</button>
-            <button
-                type="button"
-                className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
-            >Eliminar</button>
+
+            <Form
+                method='post'
+                action={`/clientes/${id}/eliminar`}
+                onSubmit={(e) =>{
+                    if(!confirm('¿Deseas eliminar este registro?')){
+                        e.preventDefault();
+                    }
+                }}
+            >
+                <button
+                    type="submit"
+                    className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+                >Eliminar</button>
+            </Form>
+
         </td>
   </tr>
   )
