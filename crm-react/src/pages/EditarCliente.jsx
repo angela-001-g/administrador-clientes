052788b985dcd720c0f6c83cd8598ecc6118ec79
angela-001-g <105566778+angela-001-g@users.noreply.github.com>
2023-08-29 20:46:42 -0500

    verificando que el user existe

--- crm-react/src/pages/EditarCliente.jsx
+++ crm-react/src/pages/EditarCliente.jsx
@@ -1,8 +1,15 @@
-export async function loader({params}){
-  
-  console.log(params)
+import { obtenerCliente } from "../data/clientes"
 
-  return ''
+export async function loader({params}){
+  const cliente = await obtenerCliente(params.clienteId)
+  if(Object.values(cliente).length === 0){
+    throw new Response('', {
+      status: 404,
+      statusText: 'No hay resultados'
+    })
+  }
+
+  return cliente
 }
 
 function EditarClientes() {
